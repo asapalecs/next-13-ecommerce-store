67ec3426abd39dedcd8ffac6293b5975485854cb
Alexandru-Robert Cristinescu <101576096+asapalecs@users.noreply.github.com>
2023-07-26 23:32:11 +0300

    Individual Category Screen

--- types.ts
+++ types.ts
@@ -1,39 +1,39 @@
-export interface Billboard{
+export interface Product {
     id: string;
-    label:string;
-    imageUrl: string;
-}
-
-export interface Category{
-    id: string;
-    name:string;
-    billboard: string;
-}
-
-export interface Product{
-    id: string;
-    name:string;
     category: Category;
+    name: string;
     price: string;
     isFeatured: boolean;
     size: Size;
     color: Color;
-    images: Image[];
-}
-
-export interface Image{
+    images: Image[]
+  };
+  
+  export interface Image {
     id: string;
     url: string;
-}
-
-export interface Size{
+  }
+  
+  export interface Billboard {
     id: string;
-    name:string;
-    value: string;
-}
-
-export interface Color{
+    label: string;
+    imageUrl: string;
+  };
+  
+  export interface Category {
     id: string;
-    name:string;
+    name: string;
+    billboard: Billboard;
+  };
+  
+  export interface Size {
+    id: string;
+    name: string;
     value: string;
-}
+  };
+  
+  export interface Color {
+    id: string;
+    name: string;
+    value: string;
+  };
